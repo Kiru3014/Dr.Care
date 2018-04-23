824458eb0d61b41faeb491f18e4e586b64e7defe
KiranGowdaa <k.gowda@boutiqaat.com>
2018-04-23 14:05:52 +0300

    help link

--- app/src/main/java/com/ecmo/android/activity/HomeActivity.java
+++ app/src/main/java/com/ecmo/android/activity/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.ecmo.android.activity;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -26,14 +29,15 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
     TextView mtxtgallery, mtxthelp, mtxtreferpatient, mtxtreferstatus;
     LinearLayout mllgallery, mllhelp, mllreferpatient, mllreferstatus;
     ImageView img_help;
-    Button mcall,logout_btn;
+    Button mcall, logout_btn;
     UserPreferences usp;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        usp=new UserPreferences(getApplicationContext());
+        usp = new UserPreferences(getApplicationContext());
 
         initviews();
         initfonts();
@@ -41,20 +45,21 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
         getSpecialitylistfromapi();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void initviews() {
         mtxtgallery = (TextView) findViewById(R.id.tv_gallery);
         mtxthelp = (TextView) findViewById(R.id.tv_help);
         mtxtreferpatient = (TextView) findViewById(R.id.tv_referpatient);
         mtxtreferstatus = (TextView) findViewById(R.id.tv_patientstatus);
         mcall = (Button) findViewById(R.id.btn_phonecall);
-        logout_btn=findViewById(R.id.logout_btn);
+        logout_btn = findViewById(R.id.logout_btn);
         mllgallery = (LinearLayout) findViewById(R.id.rv_gallery);
         mllhelp = (LinearLayout) findViewById(R.id.rv_help);
         mllreferpatient = (LinearLayout) findViewById(R.id.rv_referpatient);
         mllreferstatus = (LinearLayout) findViewById(R.id.rv_patientstatus);
-        img_help=(ImageView)findViewById(R.id.img_help);
+        img_help = (ImageView) findViewById(R.id.img_help);
 
-        if(usp.getDocType().equalsIgnoreCase("1")){
+        if (usp.getDocType().equalsIgnoreCase("1")) {
             img_help.setImageDrawable(getApplicationContext().getDrawable(R.drawable.correct));
             mtxthelp.setText("DOCTOR APPROVAL");
         }
@@ -73,11 +78,9 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
         Call<HospitalList> call = apiService.getallhospitals(new HospitalReq("getgospital"));
         call.enqueue(new Callback<HospitalList>() {
             @Override
-            public void onResponse(Call<HospitalList> call, Response<HospitalList> response)
-            {
-                HospitalList hosplist= response.body();
-                if(hosplist!=null&&hosplist.getResult().equals("SUCCESS") && hosplist.getData()!=null)
-                {
+            public void onResponse(Call<HospitalList> call, Response<HospitalList> response) {
+                HospitalList hosplist = response.body();
+                if (hosplist != null && hosplist.getResult().equals("SUCCESS") && hosplist.getData() != null) {
                     setHospitallist(hosplist.getData());
                 }
             }
@@ -94,11 +97,9 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
         Call<HospitalList> call = apiService.getallSpeciality(new HospitalReq("getspesialist"));
         call.enqueue(new Callback<HospitalList>() {
             @Override
-            public void onResponse(Call<HospitalList> call, Response<HospitalList> response)
-            {
-                HospitalList hosplist= response.body();
-                if(hosplist!=null&&hosplist.getResult().equals("SUCCESS") && hosplist.getData()!=null)
-                {
+            public void onResponse(Call<HospitalList> call, Response<HospitalList> response) {
+                HospitalList hosplist = response.body();
+                if (hosplist != null && hosplist.getResult().equals("SUCCESS") && hosplist.getData() != null) {
                     setSpecialitylist(hosplist.getData());
                 }
             }
@@ -126,10 +127,11 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
                 startActivity(new Intent(getApplicationContext(), ProfileInfoActivity.class));
                 break;
             case R.id.rv_help:
-                if(usp.getDocType().equalsIgnoreCase("1"))
-                        startActivity(new Intent(getApplicationContext(), DocListActivity.class));
+                if (usp.getDocType().equalsIgnoreCase("1"))
+                    startActivity(new Intent(getApplicationContext(), DocListActivity.class));
                 else
-                   startActivity(new Intent(getApplicationContext(), HelpActivity.class));
+                    //startActivity(new Intent(getApplicationContext(), HelpActivity.class));
+                    webview();
                 break;
             case R.id.rv_referpatient:
                 startActivity(new Intent(getApplicationContext(), PatientForm.class));
@@ -145,6 +147,13 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
                 showConfirmDialog(HomeActivity.this);
                 break;
         }
+
+    }
+
+    private void webview() {
+        // open rest of URLS in default browser
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.elso.org/resources/guidelines.aspx"));
+        startActivity(intent);
 
     }
 }
